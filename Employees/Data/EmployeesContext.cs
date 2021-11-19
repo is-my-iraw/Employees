448ebd064c10ce65b6f74f5d243f46a173566a4d
Your Name <email_cua_ban>
2021-11-19 13:59:41 +0700

    Add project files.

---
+++ Employees/Data/EmployeesContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Employees.Models;
+
+namespace Employees.Data
+{
+    public class EmployeesContext : DbContext
+    {
+        public EmployeesContext (DbContextOptions<EmployeesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employees.Models.Employee> Employee { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+
+        }
+    }
+}
---
+++ Employees/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Employees.Models
+{
+    public class Employee
+    {
+       public string EmployeeID { get; set; }
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public decimal Salary { get; set;}
+    }
+}
